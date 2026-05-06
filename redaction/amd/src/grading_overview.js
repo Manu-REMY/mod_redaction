d6c1058c3e3879063d6bb8d59e19445e95f962d4
Manu-REMY <emmanuel.remy@ac-montpellier.fr>
2026-05-06 18:42:28 +0200

    feat(overview): JS confirmation modal building blocks

--- redaction/amd/src/grading_overview.js
+++ redaction/amd/src/grading_overview.js
@@ -107,6 +107,132 @@ define([
         });
     }
 
+    function escapeHtml(s) {
+        return String(s).replace(/[&<>"']/g, function(c) {
+            return ({'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;'})[c];
+        });
+    }
+
+    function buildSummaryHtml(affected, ignored, affectedTitle, ignoredTitle) {
+        var html = '';
+        html += '<div class="mod_redaction-overview-confirm">';
+        html += '<h6 class="mod_redaction-overview-confirm-title">' + escapeHtml(affectedTitle) + '</h6>';
+        if (affected.length) {
+            html += '<ul>';
+            affected.forEach(function(a) {
+                html += '<li>' + escapeHtml(a.name) + '</li>';
+            });
+            html += '</ul>';
+        } else {
+            html += '<p><em>&mdash;</em></p>';
+        }
+        if (ignored.length) {
+            html += '<h6 class="mod_redaction-overview-confirm-title">' + escapeHtml(ignoredTitle) + '</h6>';
+            html += '<ul>';
+            ignored.forEach(function(i) {
+                html += '<li>' + escapeHtml(i.name) + ' <small>(' + escapeHtml(i.reason) + ')</small></li>';
+            });
+            html += '</ul>';
+        }
+        html += '</div>';
+        return html;
+    }
+
+    /**
+     * Partition the selected checkboxes into "affected" / "ignored" lists for a given action.
+     *
+     * @param {string} action 'reevaluate' or 'unlock'
+     * @returns {Promise<{affected: Array, ignored: Array}>}
+     */
+    function partitionForAction(action) {
+        var checks = selectedChecks();
+        var affected = [];
+        var ignored = [];
+
+        return Str.get_strings([
+            {key: 'overview_skip_reason_nocontent', component: 'mod_redaction'},
+            {key: 'overview_skip_reason_alreadyunlocked', component: 'mod_redaction'},
+        ]).then(function(strs) {
+            var reasonNoContent = strs[0];
+            var reasonAlreadyUnlocked = strs[1];
+
+            checks.forEach(function(c) {
+                var name = c.dataset.name || '';
+                var subid = parseInt(c.dataset.submissionid, 10) || 0;
+                var status = parseInt(c.dataset.status, 10) || 0;
+                var hascontent = c.dataset.hascontent === '1';
+
+                if (action === 'reevaluate') {
+                    if (hascontent) {
+                        affected.push({name: name, submissionid: subid});
+                    } else {
+                        ignored.push({name: name, reason: reasonNoContent});
+                    }
+                } else if (action === 'unlock') {
+                    if (status === 1) {
+                        affected.push({name: name, submissionid: subid});
+                    } else {
+                        ignored.push({name: name, reason: reasonAlreadyUnlocked});
+                    }
+                }
+            });
+            return {affected: affected, ignored: ignored};
+        });
+    }
+
+    /**
+     * Open the confirmation modal for a given action.
+     * Resolves with {confirmed: bool, parts: {affected, ignored}}.
+     *
+     * @param {string} action 'reevaluate' or 'unlock'
+     * @returns {Promise<{confirmed: boolean, parts: {affected: Array, ignored: Array}}>}
+     */
+    function confirmAction(action) {
+        var titleKey = action === 'reevaluate'
+            ? 'overview_confirm_reevaluate_title'
+            : 'overview_confirm_unlock_title';
+
+        return Promise.all([
+            partitionForAction(action),
+            Str.get_strings([
+                {key: titleKey, component: 'mod_redaction'},
+                {key: 'overview_confirm_button', component: 'mod_redaction'},
+            ]),
+        ]).then(function(payload) {
+            var parts = payload[0];
+            var strs = payload[1];
+            var title = strs[0];
+            var confirmLabel = strs[1];
+
+            return Str.get_strings([
+                {key: 'overview_confirm_affected', component: 'mod_redaction', param: parts.affected.length},
+                {key: 'overview_confirm_ignored', component: 'mod_redaction', param: parts.ignored.length},
+            ]).then(function(headers) {
+                var html = buildSummaryHtml(parts.affected, parts.ignored, headers[0], headers[1]);
+
+                return ModalFactory.create({
+                    type: ModalFactory.types.SAVE_CANCEL,
+                    title: title,
+                    body: html,
+                }).then(function(modal) {
+                    modal.setSaveButtonText(confirmLabel);
+                    return new Promise(function(resolve) {
+                        modal.getRoot().on(ModalEvents.save, function() {
+                            resolve({confirmed: true, parts: parts});
+                        });
+                        modal.getRoot().on(ModalEvents.cancel, function() {
+                            resolve({confirmed: false, parts: parts});
+                        });
+                        modal.getRoot().on(ModalEvents.hidden, function() {
+                            modal.destroy();
+                        });
+                        modal.show();
+                    });
+                });
+            });
+        });
+    }
+
     return {
         init: function(opts) {
             opts = opts || {};
